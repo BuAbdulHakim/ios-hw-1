4bbc08db9217c9ef0ae61e66c36b3674d5a0e142
Nasser Alsaqabi <alsokor4@gmail.com>
2020-12-24 01:26:14 +0300

    تم تعديل الملف

--- iOS-Hw1-Part1.playground/Contents.swift
+++ iOS-Hw1-Part1.playground/Contents.swift
@@ -7,7 +7,7 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+//name = "Salem"
 
 
 var myWeight = 60
@@ -15,11 +15,11 @@ var FriendWeight = 80.5
 var height = 1.5
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = myWeight + Int(FriendWeight)
 
 
 
-var bmi = myWeight * (height * height)
+var bmi = Double(myWeight) * (height * height)
 
 /*
 - - - -
@@ -38,36 +38,37 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "aaa"
+var lastName1 = "aaa"
+var email1 = "aaa@aaa.com"
+var phoneNumber1 = "12345678"
+var age1 = 10
+var country1 = "Egy"
+var area1 = "fjflg"
+var password1 = "3456789"
+var isKuwaiti1 = false
 
 //Person 2
-
-
-
-
-
-
-
-
-
+var firstName2 = "bbb"
+var lastName2 = "bbb"
+var email2 = "bbb@bbb.com"
+var phoneNumber2 = "11223344"
+var age2 = 20
+var country2 = "Kuwait"
+var area2 = "ghajfk"
+var password2 = "1234r5t"
+var isKuwaiti2 = true
 
 //Person 3
-
-
-
-
-
-
-
-
+var firstName3 = "ccc"
+var lastName3 = "cc"
+var email3 = "ccc@ccc.com"
+var phoneNumber3 = "99887766"
+var age3 = 30
+var country3 = "Saudi"
+var area3 = "hgjakl"
+var password3 = "754839iedk"
+var isKuwaiti3 = false
 
 /*
 - - - -
@@ -82,3 +83,29 @@ var isKuwaiti = false
 
  
  */
+
+age1 == age2
+age1 == age3
+age2 == age3
+
+area1 == area2
+area1 == area3
+area2 == area3
+
+age1 > 18
+age2 > 18
+age3 > 18
+
+age1 < 18
+age2 < 18
+age3 < 18
+
+age1 == 17
+age2 == 17
+age3 == 17
+
+age1 != 17
+age2 != 17
+age3 != 17
+
+print("iOS")
